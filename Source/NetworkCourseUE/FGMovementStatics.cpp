28d56c03c76783c3d19a4e0bdc781403976b2fdd
FGEbba <54099779+FGEbba@users.noreply.github.com>
2020-12-07 18:30:36 +0100

    Added the first assignment. Removed some extra files and folders.

---
+++ Source/NetworkCourseUE/FGMovementStatics.cpp
@@ -0,0 +1,17 @@
+
+#include "FGMovementStatics.h"
+
+FFGFrameMovement::FFGFrameMovement(AActor* InActor)
+{
+	StartLocation = InActor->GetActorLocation();
+}
+
+FFGFrameMovement::FFGFrameMovement(USceneComponent* InSceneComponent)
+{
+	StartLocation = InSceneComponent->GetComponentLocation();
+}
+
+void FFGFrameMovement::AddDelta(const FVector& InDelta)
+{
+	MovementDelta += InDelta;
+}
---
+++ Source/NetworkCourseUE/Player/FGPlayer.cpp
@@ -0,0 +1,151 @@
+#include "FGPlayer.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Engine/NetDriver.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/PlayerState.h"
+#include "../Components//FGMovementComponent.h"
+#include "../FGMovementStatics.h"
+
+AFGPlayer::AFGPlayer()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComponent"));
+	RootComponent = CollisionComponent;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(CollisionComponent);
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->bInheritYaw = false;
+	SpringArmComponent->SetupAttachment(CollisionComponent);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	MovementComponent = CreateDefaultSubobject<UFGMovementComponent>(TEXT("MovementComponent"));
+
+	SetReplicateMovement(false);
+}
+
+void AFGPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MovementComponent->SetUpdatedComponent(CollisionComponent);
+}
+
+void AFGPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("Accelerate"), this, &AFGPlayer::Handle_Accelerate);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &AFGPlayer::Handle_Turn);
+
+	PlayerInputComponent->BindAction(TEXT("Brake"), IE_Pressed, this, &AFGPlayer::Handle_BrakePressed);
+	PlayerInputComponent->BindAction(TEXT("Brake"), IE_Released, this, &AFGPlayer::Handle_BrakeReleased);
+}
+
+void AFGPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (IsLocallyControlled())
+	{
+		const float Friction = IsBraking() ? BrakingFriction : DefaultFriction;
+		const float Alpha = FMath::Clamp(FMath::Abs(MovementVelocity / (MaxVelocity * 0.75f)), 0.0f, 1.0f);
+		const float TurnSpeed = FMath::InterpEaseOut(0.0f, TurnSpeedDefault, Alpha, 5.0f);
+		const float MovementDirection = MovementVelocity > 0.0f ? Turn : -Turn;
+
+		Yaw += (MovementDirection * TurnSpeed) * DeltaTime;
+		FQuat WantedFacingDirection = FQuat(FVector::UpVector, FMath::DegreesToRadians(Yaw));
+		MovementComponent->SetFacingRotation(WantedFacingDirection);
+
+		FFGFrameMovement FrameMovement = MovementComponent->CreateFrameMovement();
+
+		MovementVelocity += Forward * Acceleration * DeltaTime;
+		MovementVelocity = FMath::Clamp(MovementVelocity, -MaxVelocity, MaxVelocity);
+		MovementVelocity *= FMath::Pow(Friction, DeltaTime);
+
+		MovementComponent->ApplyGravity();
+		FrameMovement.AddDelta(GetActorForwardVector() * MovementVelocity * DeltaTime);
+		MovementComponent->Move(FrameMovement);
+
+		Server_SendRotation(GetActorRotation(), DeltaTime);
+		Server_SendLocation(GetActorLocation(), DeltaTime);
+
+	}
+	else
+	{
+		if (GetActorLocation() != prevPingedLocation)
+		{
+			SetActorLocation(FMath::VInterpTo(GetActorLocation(), prevPingedLocation, PrevPingTime, TransitionTime));
+		}if (GetActorRotation() != prevPingedRotation)
+		{
+			SetActorRotation(FMath::RInterpTo(GetActorRotation(), prevPingedRotation, PrevPingTime, TransitionTime));
+		}
+	}
+}
+
+
+
+int32 AFGPlayer::GetPing() const
+{
+	if (GetPlayerState())
+	{
+		return static_cast<int32>(GetPlayerState()->GetPing());
+	}
+
+	return 0;
+}
+
+void AFGPlayer::Server_SendLocation_Implementation(const FVector& LocationToSend, float DeltaTime)
+{
+	Multicast_SendLocation(LocationToSend, DeltaTime);
+}
+
+void AFGPlayer::Multicast_SendLocation_Implementation(const FVector& LocationToSend, float DeltaTime)
+{
+	if (!IsLocallyControlled())
+	{
+		prevPingedLocation = LocationToSend;
+		PrevPingTime = DeltaTime;
+	}
+}
+
+void AFGPlayer::Server_SendRotation_Implementation(const FRotator& RotationToSend, float DeltaTime)
+{
+	Multicast_SendRotation(RotationToSend, DeltaTime);
+}
+
+void AFGPlayer::Multicast_SendRotation_Implementation(const FRotator& RotationToSend, float DeltaTime)
+{
+	if (!IsLocallyControlled())
+	{
+		prevPingedRotation = RotationToSend;
+		PrevPingTime = DeltaTime;
+	}
+}
+
+void AFGPlayer::Handle_Accelerate(float Value)
+{
+	Forward = Value;
+}
+
+void AFGPlayer::Handle_Turn(float Value)
+{
+	Turn = Value;
+}
+
+void AFGPlayer::Handle_BrakePressed()
+{
+	bBrake = true;
+}
+
+void AFGPlayer::Handle_BrakeReleased()
+{
+	bBrake = false;
+}
